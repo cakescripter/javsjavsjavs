574dbf64792d63375807dca45a11e8dff4037b1d
cakescripter <cakescripter@gmail.com>
2023-09-21 10:47:28 +0200

    commit

--- 08-music-player/script.js
+++ 08-music-player/script.js
@@ -1,7 +1,28 @@
+const img = document.querySelector('img')
+const title = document.querySelector('#title')
+const artist = document.querySelector('#artist')
 const music = document.querySelector('audio')
 const prevBtn = document.querySelector('#prev')
 const playBtn = document.querySelector('#play')
 const nextBtn = document.querySelector('#next')
+
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design',
+    },
+]
 
 let isPlaying = false;
 
@@ -18,4 +39,38 @@ function pauseSong() {
     music.pause()
 }
 
+function loadSong(song) {
+    title.textContent = song.displayName
+    artist.textContent = song.artist
+    music.src = `music/${song.name}.mp3`
+    img.src = `img/${song.name}.jpg`
+}
+
+let songIndex = 0;
+
+function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+function nextSong() {
+    songIndex++
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+
+// Bottom
+loadSong(songs[songIndex])
+
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
