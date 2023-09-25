7db380b901617f52771eb1e218fbcea295e75d13
cakescripter <cakescripter@gmail.com>
2023-09-25 12:34:53 +0200

    commit

--- 09-custom-countdown/script.js
+++ 09-custom-countdown/script.js
@@ -11,9 +11,34 @@ let countdownTitle = ''
 let countdownDate = ''
 let countdownValue = Date;
 
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0]
 dateEl.setAttribute('min', today)
+
+function updateDOM() {
+    const now = new Date().getTime()
+    const distance = countdownValue - now
+    const days = Math.floor(distance / day)
+    const hours = Math.floor((distance % day) / hour)
+    const minutes = Math.floor((distance % hour) / minute)
+    const seconds = Math.floor((distance % minute) / second)
+
+    countdownElTitle.textContent = `${countdownTitle}`
+    timeElements[0].textContent = `${days}`
+    timeElements[1].textContent = `${hours}`
+    timeElements[2].textContent = `${minutes}`
+    timeElements[3].textContent = `${seconds}`
+
+    inputContainer.hidden = true
+    countdownEl.hidden = false
+}
+
+
 
 // Form Handler
 function updateCountdown(e) {
@@ -21,7 +46,8 @@ function updateCountdown(e) {
     countdownTitle = e.srcElement[0].value
     countdownDate = e.srcElement[1].value
 
-    countdownValue = new dateEl(countdownDate).getTime()
+    countdownValue = new Date(countdownDate).getTime()
+    updateDOM()
 }
 
 // Event Listeners
