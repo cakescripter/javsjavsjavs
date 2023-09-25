dbbf5d699d2254c2ad68976b143e391c2242f53b
cakescripter <cakescripter@gmail.com>
2023-09-25 12:48:22 +0200

    commit

--- 09-custom-countdown/script.js
+++ 09-custom-countdown/script.js
@@ -10,6 +10,7 @@ const timeElements = document.querySelectorAll('span')
 let countdownTitle = ''
 let countdownDate = ''
 let countdownValue = Date;
+let countdownActive
 
 const second = 1000
 const minute = second * 60
@@ -21,21 +22,24 @@ const today = new Date().toISOString().split('T')[0]
 dateEl.setAttribute('min', today)
 
 function updateDOM() {
-    const now = new Date().getTime()
-    const distance = countdownValue - now
-    const days = Math.floor(distance / day)
-    const hours = Math.floor((distance % day) / hour)
-    const minutes = Math.floor((distance % hour) / minute)
-    const seconds = Math.floor((distance % minute) / second)
+    countdownActive = setInterval(() => {
+        const now = new Date().getTime()
+        const distance = countdownValue - now
 
-    countdownElTitle.textContent = `${countdownTitle}`
-    timeElements[0].textContent = `${days}`
-    timeElements[1].textContent = `${hours}`
-    timeElements[2].textContent = `${minutes}`
-    timeElements[3].textContent = `${seconds}`
+        const days = Math.floor(distance / day)
+        const hours = Math.floor((distance % day) / hour)
+        const minutes = Math.floor((distance % hour) / minute)
+        const seconds = Math.floor((distance % minute) / second)
 
-    inputContainer.hidden = true
-    countdownEl.hidden = false
+        countdownElTitle.textContent = `${countdownTitle}`
+        timeElements[0].textContent = `${days}`
+        timeElements[1].textContent = `${hours}`
+        timeElements[2].textContent = `${minutes}`
+        timeElements[3].textContent = `${seconds}`
+
+        inputContainer.hidden = true
+        countdownEl.hidden = false
+    }, second)
 }
 
 
@@ -50,5 +54,18 @@ function updateCountdown(e) {
     updateDOM()
 }
 
+// Reset All Values
+function reset() {
+    //Hide Countdowns, show Input
+    countdownEl.hidden = true
+    inputContainer.hiden = false
+    //Stop the countdown
+    clearInterval(countdownActive)
+    // Reset values
+    let countdownTitle = ''
+    let countdownDate = ''
+}
+
 // Event Listeners
 countdownForm.addEventListener('submit', updateCountdown)
+countdownBtn.addEventListener('click', reset)
