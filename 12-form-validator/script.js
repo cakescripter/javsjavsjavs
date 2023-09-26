eb5ce10abdc4ae6c59867ff356892e227ae407e6
cakescripter <cakescripter@gmail.com>
2023-09-26 13:09:11 +0200

    commit

---
+++ 12-form-validator/script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('#form')
+const password1El = document.querySelector('#password1')
+const password2El = document.querySelector('#password2')
+const messageContainer = document.querySelector('#message-container')
+const message = document.querySelector('#message')
+
+function processFormData(e) {
+
+}
+
+// Event Listener
+form.addEventListener('submit', processFormData)
