77de7a4feb7842e112acdbfb6a5624c6b022016f
cakescripter <cakescripter@gmail.com>
2023-09-26 12:16:02 +0200

    commit

--- 10-book-keeper/script.js
+++ 10-book-keeper/script.js
@@ -33,6 +33,68 @@ function validate(nameValue, urlValue) {
     return true
 }
 
+// Build Bookmarks DOM
+function buildBookmarks() {
+    // Remove all bookmark elements
+    bookmarksContainer.textContent = ''
+    // Build items
+    bookmarks.forEach((bookmark) => {
+        const { name, url } = bookmark
+        // Item
+        const item = document.createElement('div')
+        item.classList.add('item')
+        // Close Icon
+        const closeIcon = document.createElement('i')
+        closeIcon.classList.add('fas', 'fa-times')
+        closeIcon.setAttribute('title', 'Delete Bookmark')
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`)
+        // Favicon / Link Container
+        const linkInfo = document.createElement('div')
+        linkInfo.classList.add('name')
+        // Favicon
+        const favicon = document.createElement('img')
+        favicon.setAttribute('src', `httpss://s2.googleusercontent.com/s2/favicons?domain=${url}`)
+        favicon.setAttribute('alt', 'Favicon')
+        // Link
+        const link = document.createElement('a')
+        link.setAttribute('href', `${url}`)
+        link.setAttribute('target', '_blank')
+        link.textContent = name
+        // Append to bookmarks container
+        linkInfo.append(favicon, link)
+        item.append(closeIcon, linkInfo)
+        bookmarksContainer.appendChild(item)
+    })
+}
+
+// Fetch Bookmarks from LS
+function fetchBookmarks() {
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+    } else {
+        bookmarks = [
+            {
+                name: 'xdy',
+                url: 'https://google.com'
+            }
+        ]
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    }
+    buildBookmarks()
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+    bookmarks.forEach((bookmark, i) => {
+        if (bookmark.url === url) {
+            bookmarks.splice(i, 1)
+        }
+    })
+    // Update bookmarks in LS
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    fetchBookmarks()
+}
+
 // Handle Data from Form
 function storeBookmark(e) {
     e.preventDefault()
@@ -51,7 +113,8 @@ function storeBookmark(e) {
         url: urlValue,
     }
     bookmarks.push(bookmark)
-
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    fetchBookmarks()
     bookmarkForm.reset()
     websiteNameEl.focus()
 }
@@ -59,3 +122,6 @@ function storeBookmark(e) {
 
 // Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark)
+
+// On Load
+fetchBookmarks()
